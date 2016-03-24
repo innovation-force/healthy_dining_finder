6b69c2b59a499ac0ae83d6498c666c541e492799
Joe Landry <joe.landry@cambiahealth.com>
2016-03-24 13:03:04 -0700

    class method syntax method

--- lib/healthy_dining_finder.rb
+++ lib/healthy_dining_finder.rb
@@ -10,11 +10,13 @@ module HDF
   @@logger = Logger.new STDOUT
   @@logger.level = Logger::WARN
 
-  def self.read_json(json_file)
+  class << self
+
+  def read_json(json_file)
       json_response(File.read(json_file))
   end
 
-  def self.all_clients
+  def all_clients
       target_url = URI("#{BASE_URL}AllClientJSON.svc/client/#{TOKEN}")
       result = Net::HTTP.get_response(target_url)
       if result.code == '200'
@@ -26,7 +28,7 @@ module HDF
       end
   end
 
-  def self.client_info(client_code)
+  def client_info(client_code)
       target_url = URI("#{BASE_URL}GetClientJSON.svc/Client/#{TOKEN}/#{client_code}")
       result = Net::HTTP.get_response(target_url)
       if result.code == '200'
@@ -38,7 +40,7 @@ module HDF
       end
   end
 
-  def self.client_dishes_all_details(client_code)
+  def client_dishes_all_details(client_code)
       target_url = URI("#{BASE_URL}ClientDishesAllDetailsJSON.svc/Client/#{TOKEN}/#{client_code}")
       result = Net::HTTP.get_response(target_url)
       if result.code == '200'
@@ -50,7 +52,7 @@ module HDF
       end
   end
 
-  def self.client_dishes_all_details_by_radius(address, radius)
+  def client_dishes_all_details_by_radius(address, radius)
       address_encoded = URI.encode(address)
       target_url = URI("#{BASE_URL}ClientDishesAllDetailsByAddressRadiusJSON.svc/dish/#{TOKEN}/#{address_encoded}/#{radius}")
       result = Net::HTTP.get_response(target_url)
@@ -63,7 +65,7 @@ module HDF
         end
   end
 
-  def self.restaurants_by_location(address)
+  def restaurants_by_location(address)
       address_encoded = URI.encode(address)
       target_url = URI("#{BASE_URL}RestaurantLocationsByZipCodeCityStateJSON.svc/RESTAURANT/#{TOKEN}/allrestaurants/#{address_encoded}/6")
       result = Net::HTTP.get_response(target_url)
@@ -76,7 +78,7 @@ module HDF
         end
   end
 
-  def self.dishes_by_sodium(address, keyword, sodium, radius)
+  def dishes_by_sodium(address, keyword, sodium, radius)
       address_encoded = URI.encode(address)
       target_url = URI("#{BASE_URL}/DishesBySodiumbyAddressRadiusJSON.svc/dish/sodium/#{TOKEN}/#{address_encoded}/#{keyword}/#{sodium}/#{radius}")
       result = Net::HTTP.get_response(target_url)
@@ -89,4 +91,5 @@ module HDF
         end
   end
 
+  end
 end
